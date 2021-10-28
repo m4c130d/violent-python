281117267e2c427489d04eeea9526dcd424f783f
kali <kali@kali>
2021-10-28 19:48:24 +0200

    added thread module

--- chapter2/scanner.py
+++ chapter2/scanner.py
@@ -1,5 +1,8 @@
 import argparse
 import socket
+import threading
+
+screen_lock = threading.Semaphore(1)
 
 def conn_scan(ip, port):
     try:
@@ -7,12 +10,15 @@ def conn_scan(ip, port):
         conn_skt.connect((ip, port))
         conn_skt.send('Dataaaaa\r\n'.encode())
         results = conn_skt.recv(100)
+        screen_lock.acquire()
         print(f'[+] {port}/tcp ouvert')
         print(f'[+] {results}')
-        conn_skt.close()
     except Exception as e:
+        screen_lock.acquire()
         print(f'[-] {port}/tcp ferme : {e}')
-
+    finally:
+        screen_lock.release()
+        conn_skt.close()
 
 
 
@@ -29,9 +35,8 @@ def port_scan(dest, port):
         print(f'[+] Resutlat de scan pour : {ip}')
     socket.setdefaulttimeout(1)
     for ports in port:
-        print(f' Scanning port {ports}')
-        conn_scan(ip, int(ports))
-
+        t = threading.Thread(target=conn_scan, args=(ip, int(ports)))
+        t.start()
 
 
 
@@ -41,9 +46,10 @@ def main():
                                     formatter_class=argparse.RawDescriptionHelpFormatter,
                                     usage = 'scanner.py -d <tgtHost> -p <tgtPort>')
     parser.add_argument('-d','--dest', help='Destination Hostname', required=True)
-    parser.add_argument('-p','--port', help='Destination Port', required=True, nargs='+')
+    parser.add_argument('-p','--port', help='Destination Port', required=True, type=str)
     args = parser.parse_args()
-    port_scan(args.dest, args.port)
+    ports_parser = args.port.split(",")
+    port_scan(args.dest, ports_parser)
 
 if __name__ == '__main__':
     main()
